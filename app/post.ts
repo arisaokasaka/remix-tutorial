cc6dab0d90408782737c530188074c85f70ae865
Arisa Okasaka <68284764+arisaokasaka@users.noreply.github.com>
2021-12-12 21:22:51 +0900

    dynamic route

--- app/post.ts
+++ app/post.ts
@@ -2,6 +2,7 @@ import path from "path";
 import fs from "fs/promises";
 import parseFrontMatter from "front-matter";
 import invariant from "tiny-invariant";
+import { marked } from "marked"; // markdownをhtmlに変換するライブラリ
 
 export type Post = {
   slug: string;
@@ -22,27 +23,16 @@ function isValidPostAttributes(
   return attributes?.title;
 }
 
-export async function getPosts() {
-  const dir = await fs.readdir(postsPath);
-  return Promise.all(
-    dir.map(async filename => {
-      const file = await fs.readFile(
-        path.join(postsPath, filename)
-      );
-      const { attributes } = parseFrontMatter(
-        file.toString()
-      );
+export async function getPost(slug: string) {
+  const filepath = path.join(postsPath, slug + ".md");
+  const file = await fs.readFile(filepath);
+  const { attributes, body } = parseFrontMatter(file.toString());
 
-      // isValidPostAttributesがfalseであればエラーを返す
-      invariant(
-        isValidPostAttributes(attributes),
-        `${filename} has bad meta data!`
-      );
-      
-      return {
-        slug: filename.replace(/\.md$/, ""),
-        title: attributes.title,     
-      };
-    })
+  invariant(
+    isValidPostAttributes(attributes),
+    `Post ${filepath} is missing attributes`
   );
+
+  const html = marked(body);
+  return { slug, html, title: attributes.title };
 }
